225df417b79ba093a86a444560dca5f0fff6527f
daioooor36 <pd6016@naver.com>
2019-06-30 22:06:40 +0900

    fix(BranchCreator) : Interface 적용, 상수 static적용

--- src/main/java/nextstep/ladder/model/BranchCreator.java
+++ src/main/java/nextstep/ladder/model/BranchCreator.java
@@ -1,15 +1,5 @@
 package nextstep.ladder.model;
 
-import java.util.Random;
-
-public class BranchCreator {
-    private static Random random = new Random();
-
-    BranchCreator() {
-        random = new Random();
-    }
-
-    public static boolean createBranchRandomly() {
-        return random.nextBoolean();
-    }
+public interface BranchCreator {
+    boolean createBranch();
 }
